ff5451fecbe3c5675ec880dc8719df9be921e592
akira-kishimoto <xbn06314@gmail.com>
2026-07-22 17:54:33 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "english-flashcards-v1";
+
+const urlsToCache = [
+    "./",
+    "./index.html",
+    "./cards.js",
+    "./manifest.json",
+    "./icon-192.png",
+    "./icon-512.png"
+];
+
+self.addEventListener("install",event=>{
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+
+        .then(cache=>cache.addAll(urlsToCache))
+
+    );
+
+});
+
+self.addEventListener("fetch",event=>{
+
+    event.respondWith(
+
+        caches.match(event.request)
+
+        .then(response=>{
+
+            return response || fetch(event.request);
+
+        })
+
+    );
+
+});
